f06d6704b79d40ac55403b911cad25db24236b67
Ramzey <remzesii@gmail.com>
2022-05-21 04:28:43 +0500

    Сделано первое задание

---
+++ ex001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Func(int b){
+    
+    int d = b/10%10;
+
+    return d;
+    
+}
+
+Console.WriteLine("Пожалуйста введите любое трехзначное число: ");
+string a = Console.ReadLine();
+int b = int.Parse(a);
+
+
+while (b/100 >= 9 || b/100 == 0){
+    Console.WriteLine("Вы ввели неправильное число! Пожалуста введите трехзначное число: ");
+    a = Console.ReadLine();
+    b = int.Parse(a);
+}
+int c = Func(b);
+    Console.WriteLine($"Вторая цифра {b} это {c}");
+
+
+
+
+
+
+
+
+
+
